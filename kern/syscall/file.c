560a2ae28fcf7ea765d513940c9a8582a3b3ff0d
Dan Sun <d.y.sun@student.unsw.edu.au>
2019-03-29 18:12:32 +1100

    need to commit so i can pull viky's changes

--- kern/syscall/file.c
+++ kern/syscall/file.c
@@ -34,68 +34,10 @@ ssize_t sys_read(int fd, void *buf, size_t nbytes, int* err) {
         *err = EFAULT;
         return -1;
     }
+    
+    
 
-    // check of_table
-	struct file_descriptor * fdp = &(curproc->fd_table[fd]);
-    if(of_table[fdp->index] == NULL) {
-        *err = EBADF;
-        return -1;
-    }
 
-	//Check the premission
-	if(fdp->mode == O_WRONLY) {
-        *err = EBADF;
-        return -1;
-    }
-
-	struct opened_file * of = of_table[fdp->index];
-
-	//for debug
-	KASSERT(of != NULL);
-
-	void * kbuf = kmalloc(sizeof(void *) * nbytes);
-	if(kbuf == NULL) {
-        *err = EFAULT;
-        return -1;
-    }
-
-	struct iovec _iovec;
-	struct uio _uio;
-	int result;
-
-	_iovec.iov_ubase = (userptr_t) buf;
-
-    //lock offset and start reading
-	lock_acquire(of->of_lock);
-
-	uio_kinit(&_iovec, &_uio, kbuf, nbytes, of->of_offset, UIO_READ);
-
-	result = VOP_READ(of->of_vnode, &_uio);
-
-	if (result)
-	{
-		lock_release(of->of_lock);
-		kfree(kbuf);
-        *err = result;
-		return -1;
-	}
-
-    // the length of read
-    int len = nbytes - _uio.uio_resid;
-    // copy the kernal buffer to userland buffer
-    result = copyout(kbuf, buf, len);
-
-	kfree(kbuf);
-    if (result && len) {
-        lock_release(of->of_lock);
-        *err = result;
-        return -1;
-    }
-    // update offset
-	of->of_offset += len;
-	lock_release(of->of_lock);
-
-	return len;
 }
 
  
